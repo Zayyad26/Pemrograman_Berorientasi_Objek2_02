62a862846be5804fccb66bf7b1485d17a0290cbf
Zayyad <128308632+Zayyad26@users.noreply.github.com>
2023-03-27 09:20:36 +0700

    Create Hierarchical1.py

---
+++ Praktikum 2/Hierarchical1.py
@@ -0,0 +1,35 @@
+class Animal:
+    def _init_(self, name, color):
+        self.name = name
+        self.color = color
+    def get_name(self):
+        return self.name
+    def get_color(self):
+        return self.color
+    
+class Mammal(Animal):
+    def _init_(self, name, color, fur):
+        super()._init_(name, color)
+        self.fur = fur
+    def get_fur(self):
+        return self.fur
+    
+class Cat(Animal):
+    def _init_(self, name, color):
+        super()._init_(name)
+        self.color = color
+    def get_color(self):
+        return self.color
+    
+class Reptile(Mammal):
+    def _init_(self, name, color, age):
+        super()._init_(name, color, age)
+        self.age = age
+    def get_details(self):
+        print(f"Name : {self.name}")
+        print(f"Color : {self.color}")
+        print(f"Age : {self.age}")
+        return self.age
+    
+animalA = Reptile("Dog", "White", 4)
+animalA.get_details()
