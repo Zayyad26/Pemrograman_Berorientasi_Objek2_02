fa5bfc86e96e16a732a6ca8a4051b981742dcad6
Zayyad <128308632+Zayyad26@users.noreply.github.com>
2023-03-27 08:45:56 +0700

    Update Single1.py

--- Praktikum 2/Single1.py
+++ Praktikum 2/Single1.py
@@ -1,1 +1,19 @@
+class Binatang:
+    def __init__(self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+    
+    def bergerak(self):
+        print(self.nama, "Berlari")
 
+class Kuda(Binatang):
+    def __init__(self, nama, umur, jenis_kuda):
+        super().__init__(nama, umur)
+        self.jenis_kuda = jenis_kuda
+    
+    def bersuara(self):
+        print(f"{self.nama} Yang Berumur {self.umur} Tahun Dengan Jenis {self.jenis_kuda} Berlari dengan cepat")
+
+kudaA = Kuda("Flawless", 3, "Morgan")
+kudaA.bergerak()
+kudaA.bersuara()
